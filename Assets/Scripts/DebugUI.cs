f564021923724bf97330370fb12a97150e8685df
dexyr <61259802+Dexyr@users.noreply.github.com>
2024-11-23 19:34:40 +0900

    2日目(続き2)

--- Assets/Scripts/DebugUI.cs
+++ Assets/Scripts/DebugUI.cs
@@ -3,24 +3,32 @@ using UnityEngine.UIElements;
 
 public class DebugUI : MonoBehaviour {
     GameController gameController;
-    Label debug;
+    Label debug, state;
 
     void Awake() {
         var controllerObject = GameObject.FindGameObjectWithTag("GameController");
         gameController = controllerObject.GetComponent<GameController>();
 
         gameController.LogUpdated += UpdateLogText;
+        gameController.StateChanged += UpdateStateText;
 
         var uiDocument = GetComponent<UIDocument>();
         debug = (Label) uiDocument.rootVisualElement.Query("debug");
+        state = (Label) uiDocument.rootVisualElement.Query("state");
     }
 
     public void OnDestroy() {
-        if (gameController)
+        if (gameController) {
             gameController.LogUpdated -= UpdateLogText;
+            gameController.StateChanged -= UpdateStateText;
+        }
     }
 
     public void UpdateLogText(string message) {
         debug.text += "\n" + message;
+    }
+
+    public void UpdateStateText(GameController.State gameState) {
+        state.text = $"(åªç›èÛë‘)\n{gameState}";
     }
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,9 +2,25 @@ using UnityEngine;
 using UnityEngine.Events;
 
 public class GameController : MonoBehaviour {
+    public enum State { MENU, SHOT, SIMULATION, FREE, END };
+
     public delegate void LogUpdateHandler(string message);
     public event LogUpdateHandler LogUpdated;
-    
+
+    public delegate void StateChangeHandler(State state);
+    public event StateChangeHandler StateChanged;
+
+    State state;
+    // propertyのほうがいいかな
+    public void changeState(State newState) {
+        state = newState;
+        StateChanged?.Invoke(state);
+    }
+
+    public void Start() {
+        changeState(State.SIMULATION);
+    }
+
     public void CueBallCollided(Ball ball) {
         Ball.Group group = Ball.GetGroup(ball.number);
         LogUpdated?.Invoke($"ボール{ball.number}(group {group})がキューボールに当てった");
@@ -13,5 +29,7 @@ public class GameController : MonoBehaviour {
     public void PocketEntered(Ball ball) {
         Ball.Group group = Ball.GetGroup(ball.number);
         LogUpdated?.Invoke($"ボール{ball.number}(group {group})がクッションに当てった");
+
+        changeState(State.END);
     }
 }
